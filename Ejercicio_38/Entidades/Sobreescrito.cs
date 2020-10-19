815a9fef3c6376db88428edb826961b371cd1902
Nicolas <nicolaseduardoperez@gmail.com>
2020-10-19 00:46:16 -0300

    ejercicios guia

---
+++ Ejercicio_38/Entidades/Sobreescrito.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sobreescrito
+    {
+        public override string ToString()
+        {
+            return "¡Este es mi metodo ToString sobreescrito!";
+        }
+        public override bool Equals(object obj)
+        {
+            return this==obj;
+        }
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
---
+++ Ejercicio_39/Entidades/Sobreescrito.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Sobreescrito
+    {
+        protected string miAtributo;
+
+        public Sobreescrito()
+        {
+            this.miAtributo = "Probar Abstractos";
+        }
+        public string MiAtributo
+        {
+            get
+            {
+                return this.miAtributo;
+            }
+        }
+        public abstract string MiMetodo();
+        public override string ToString()
+        {
+            return "¡Este es mi metodo ToString sobreescrito!";
+        }
+        public override bool Equals(object obj)
+        {
+            return this==obj;
+        }
+        public override int GetHashCode()
+        {
+            return 1142510187;
+        }
+    }
+}
